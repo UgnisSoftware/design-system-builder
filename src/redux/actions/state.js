55c5daea29f4d4f685eee576efb9bb9eb3d5d8cc
UgnisSoftware <karolis.masiulis@ugnis.com>
2016-09-04 20:56:18 +0300

    Remove old files

--- src/redux/actions/state.js
+++
@@ -1,7 +0,0 @@
-export const ADD_STATE = 'ADD_STATE'
-
-export function onAddState(name, value) {
-    return (dispatch, getState)=> {
-        dispatch({type: ADD_STATE, name, value})
-    }
-}
--- src/redux/actions/ui.js
+++
@@ -1,7 +0,0 @@
-export const BG_CHANGE = 'BG_CHANGE'
-
-export function onChangeBgColor(color) {
-    return (dispatch, getState)=> {
-        dispatch({type: BG_CHANGE, color, selectedNodeId: getState().ui.selectedNodeId})
-    }
-}
