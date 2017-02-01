c12ff2755aa06e373f2a428132b74bfe20ccf6fb
UgnisSoftware <karolis.masiulis@ugnis.com>
2017-02-02 01:21:30 +0200

    Add website and prod task

---
+++ server.prod.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const spawn = require('child_process').spawn
+
+app.use(bodyParser.json())
+app.use(express.static('./static_prod'))
+
+app.post('/_github_update', (req, res)=> {
+    spawn('git pull')
+})
+
+app.listen(3000, ()=> {
+    console.log('Listening on port 3000')
+})
