47f456580b3fabe1e8f845e16d6c9d62834057c9
masiulis <karolis.masiulis@ugnis.com>
2017-05-03 12:05:53 +0300

    comment out app saving

--- server.dev.js
+++ server.dev.js
@@ -7,16 +7,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./static_prod'))
 
-app.post('/save', (req, res)=> {
-    fs.writeFile("./ugnis_components/app-" + uuid.v4() + '.json', JSON.stringify(req.body, undefined, 4), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-
-        console.log("The file was saved!");
-    });
-    res.send('OK')
-})
+// app.post('/save', (req, res)=> {
+//     fs.writeFile("./ugnis_components/app-" + uuid.v4() + '.json', JSON.stringify(req.body, undefined, 4), function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//
+//         console.log("The file was saved!");
+//     });
+//     res.send('OK')
+// })
 
 app.listen(3000, ()=> {
     console.log('Listening on port 3000')
