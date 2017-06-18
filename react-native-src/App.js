1b0d54852f16a20a4bd2cd81788f52c79dc6441b
UgnisSoftware <karolis.masiulis@ugnis.com>
2017-06-18 05:01:59 +0300

    Add react native

--- react-native-src/App.js
+++ react-native-src/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shakeoper menu.</Text>
+        <Text>Shakeoper hhjbj.</Text>
       </View>
     );
   }
