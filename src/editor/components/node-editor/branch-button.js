6c6e2c7e4ed372ac7bfec13af58f273d48e21082
UgnisSoftware <karolis.masiulis@ugnis.com>
2017-09-16 05:25:15 +0300

    Clicking branch button adds more branches

--- src/editor/components/node-editor/branch-button.js
+++ src/editor/components/node-editor/branch-button.js
@@ -4,9 +4,68 @@ import R from 'ramda'
 import { IfIcon } from '../icons'
 import { uuid } from '../../utils'
 
+function ADD_NEW_BRANCH(splitRef) {
+
+    const defaultPipeRef = state.definitionList[state.currentDefinitionId][splitRef.ref][splitRef.id].defaultValue
+    const defaultPipe = state.definitionList[state.currentDefinitionId][defaultPipeRef.ref][defaultPipeRef.id]
+
+    const newBranchId = uuid()
+    const newTestPipeId = uuid()
+    const newValuePipeId = uuid()
+
+    const newBranch = {
+        test: {
+            ref: 'pipe',
+            id: newTestPipeId,
+        },
+        value: {
+            ref: 'pipe',
+            id: newValuePipeId,
+        },
+    }
+    const newPipes = {
+        [newTestPipeId]: {
+            type: 'boolean',
+            value: true,
+            transformations: [],
+        },
+        [newValuePipeId]: {
+            type: defaultPipe.type,
+            value: defaultPipe.value,
+            transformations: [],
+        },
+    }
+
+    setState(
+        R.evolve({
+            definitionList: {
+                [state.currentDefinitionId]: {
+                    pipe: R.merge(newPipes),
+                    branch: R.assoc(newBranchId, newBranch),
+                    split: {
+                        [splitRef.id]: {
+                            branches: R.append({
+                                ref: 'branch',
+                                id: newBranchId,
+                            })
+                        }
+                    },
+                },
+            },
+        })(state)
+    )
+}
+
 function BRANCH_PIPE(ref, propertyName) {
+
     const node = state.definitionList[state.currentDefinitionId][ref.ref][ref.id]
     const oldPipeRef = node[propertyName]
+
+    // if it's already a split, just add a new branch
+    if(oldPipeRef.ref === 'split'){
+        return ADD_NEW_BRANCH(oldPipeRef)
+    }
+
     const oldPipe = state.definitionList[state.currentDefinitionId][oldPipeRef.ref][oldPipeRef.id]
 
     const newBranchId = uuid()
@@ -37,13 +96,13 @@ function BRANCH_PIPE(ref, propertyName) {
         [newTestPipeId]: {
             type: 'boolean',
             value: true,
-            transformations: []
+            transformations: [],
         },
         [newValuePipeId]: {
             type: oldPipe.type,
             value: oldPipe.value,
-            transformations: []
-        }
+            transformations: [],
+        },
     }
 
     setState(
@@ -54,11 +113,11 @@ function BRANCH_PIPE(ref, propertyName) {
                     branch: R.assoc(newBranchId, newBranch),
                     split: R.assoc(newSplitId, newSplit),
                     [ref.ref]: {
-                        [ref.id]:R.assoc(propertyName, {
+                        [ref.id]: R.assoc(propertyName, {
                             ref: 'split',
-                            id: newSplitId
+                            id: newSplitId,
                         }),
-                    }
+                    },
                 },
             },
         })(state)
