7a30a5d1903b63bc5d166acf5fad9655d96801bf
masiulis <karolis.masiulis@ugnis.com>
2018-07-01 17:54:52 +0300

    Fix scroll

--- src/editor/Center/Center.tsx
+++ src/editor/Center/Center.tsx
@@ -5,7 +5,7 @@ import TopBar from './TopBar/TopBar';
 import Fonts from './Fonts/Fonts';
 
 const Center = styled.div`
-  box-shadow: rgba(0, 0, 0, 0.12) 2px 2px 2px;
+  overflow: scroll;
   padding: 0 24px;
   flex: 1 1 auto;
 `;
