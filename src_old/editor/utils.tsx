1248fde04a409b514071e1df33b1375593239888
masiulis <karolis.masiulis@ugnis.com>
2018-07-03 16:15:10 +0300

    Remove ugnis 0.9.2

--- src_old/editor/utils.tsx
+++
@@ -1,7 +0,0 @@
-// Uteles
-export function uuid() {
-    //return ('' + 1e7 + -1e3 + -4e3 + -8e3 + -1e11).replace(/[10]/g, function() {
-    return ('' + 1e7).replace(/[10]/g, function() {
-        return (0 | (Math.random() * 16)).toString(16)
-    })
-}
