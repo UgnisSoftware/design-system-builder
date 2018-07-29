974c441086ca331a3e3e3a1995d1a17def705b01
masiulis <karolis.masiulis@ugnis.com>
2018-07-29 15:31:37 +0300

    Improve mock content

--- src/editor/Center/Preview/ComponentView/CenterWithTopAndBottom.tsx
+++ src/editor/Center/Preview/ComponentView/CenterWithTopAndBottom.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { RootNode } from '@src/interfaces';
 import styled from 'styled-components';
+
+import { RootNode } from '@src/interfaces';
+
 import Component from './_Component';
 
 interface Props {
@@ -15,21 +17,55 @@ const CenterComponent = styled.div`
   flex-direction: column;
 `;
 
-const ContentTop = styled.div`
-  flex: 1;
-  background: rgba(0,0,0, 0.25);
-  width: 100%;
+const ContentLoaderWrapperTop = styled.div`
+  flex: 1 1 auto;
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  overflow: hidden;
 `;
-const ContentBottom = styled.div`
-  flex: 1;
-  background: rgba(0,0,0, 0.25);
-  width: 100%;
+
+const ContentLoaderWrapperBottom = styled(ContentLoaderWrapperTop)`
+  justify-content: flex-start;
 `;
+
+const ComponentWrapper = styled.div`
+  flex: 0 0 auto;
+`;
+
+const ContentTop = () => (
+  <ContentLoaderWrapperTop>
+    <svg height="auto" width="100%" fill="#cecece" viewBox="0 0 380 156">
+      <rect x="70" y="45" rx="4" ry="4" width="117" height="6.4" />
+      <rect x="70" y="65" rx="3" ry="3" width="85" height="6.4" />
+      <rect x="0" y="110" rx="3" ry="3" width="350" height="6.4" />
+      <rect x="0" y="130" rx="3" ry="3" width="380" height="6.4" />
+      <rect x="0" y="150" rx="3" ry="3" width="201" height="6.4" />
+      <circle cx="30" cy="60" r="30" />
+    </svg>
+  </ContentLoaderWrapperTop>
+);
+
+const ContentBottom = () => (
+  <ContentLoaderWrapperBottom>
+    <svg height="auto" width="100%"  fill="#cecece" viewBox="0 0 380 156">
+      <rect x="0" y="0" rx="3" ry="3" width="350" height="6.4" />
+      <rect x="0" y="20" rx="3" ry="3" width="380" height="6.4" />
+      <rect x="0" y="40" rx="3" ry="3" width="380" height="6.4" />
+      <rect x="0" y="60" rx="3" ry="3" width="380" height="6.4" />
+      <rect x="0" y="80" rx="3" ry="3" width="201" height="6.4" />
+    </svg>
+  </ContentLoaderWrapperBottom>
+);
 
 export default ({ component }: Props) => (
   <CenterComponent>
     <ContentTop />
-    <Component component={component} />
+    <ComponentWrapper>
+      <Component component={component} />
+    </ComponentWrapper>
     <ContentBottom />
   </CenterComponent>
 );
