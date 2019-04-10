7093fec09cfb857abbd7131be0ebef14bdf5c459
karolismasiulis <karolis.masiulis@revolut.com>
2019-04-10 14:08:44 +0300

    Revert "Remove pages"
    This reverts commit de67bd3a1cb8d4d5eb0a3fa642033c422c2d7057.

--- src/selectors.ts
+++ src/selectors.ts
@@ -5,4 +5,7 @@ export const getCurrentComponent = () => {
   if (state.ui.router.path === RouterPaths.component) {
     return state.components[state.ui.router.componentId]
   }
+  if (state.ui.router.path === RouterPaths.page) {
+    return state.pages[state.ui.router.componentId]
+  }
 }
