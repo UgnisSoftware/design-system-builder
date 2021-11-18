0af49c451bdd51f1f84cb2dfd51a67220d6f277c
masiulis <karolis.masiulis@ugnis.com>
2021-11-18 14:16:53 +0200

    Update Table size calculation

--- src/components/TableEditable/src/TableEditable.tsx
+++ src/components/TableEditable/src/TableEditable.tsx
@@ -1,4 +1,4 @@
-import DataGrid, { DataGridProps } from "react-data-grid"
+import DataGrid, { DataGridHandle, DataGridProps } from "react-data-grid"
 import { useEffect, useRef, useState } from "react"
 
 interface Props<Data> extends DataGridProps<Data> {
@@ -9,23 +9,22 @@ const ROW_HEIGHT = 35
 
 export function TableEditable<Data extends {}>({ columns, onRowClick, ...props }: Props<Data>) {
   const [height, setHeight] = useState(window.innerHeight)
-  const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<DataGridHandle>(null)
 
   useEffect(() => {
-    setHeight(ref?.current?.clientHeight || window.innerHeight)
+    setHeight(ref?.current?.element?.clientHeight || window.innerHeight)
   }, [])
 
   const tableHeight = Math.min(height, props.rows.length * (props.rowHeight || ROW_HEIGHT))
 
   return (
-    <div ref={ref}>
-      <DataGrid
-        columns={columns}
-        onRowClick={onRowClick}
-        style={{ height: tableHeight }}
-        rowHeight={ROW_HEIGHT}
-        {...props}
-      />
-    </div>
+    <DataGrid
+      ref={ref}
+      columns={columns}
+      onRowClick={onRowClick}
+      style={{ height: tableHeight }}
+      rowHeight={ROW_HEIGHT}
+      {...props}
+    />
   )
 }
